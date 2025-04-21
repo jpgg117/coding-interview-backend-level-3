9fd020fd9020a751cf9d1d959325475cc146fd19
juanpablogarcia <juanpablogarcia78@hotmail.com>
2025-04-20 22:10:54 -0500

    adding validators and test for validations

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import {ServerRoute} from '@hapi/hapi';
 import * as ItemService from './services/item.service';
+import { crudItemSchema } from "./validators/item.validator";
 
 export const defineRoutes: ServerRoute[] = [
     {
@@ -19,15 +20,6 @@ export const defineRoutes: ServerRoute[] = [
         }
     },
     {
-        method: 'POST',
-        path: '/items',
-        handler: async (request, h) => {
-            const { name, price } = request.payload as { name: string; price: number };
-            const item = await ItemService.createItem(name, price);
-            return h.response(item).code(201);
-        }
-    },
-    {
         method: 'GET',
         path: '/items/{id}',
         handler: async (request, h) => {
@@ -42,19 +34,48 @@ export const defineRoutes: ServerRoute[] = [
         }
     },
     {
+        method: 'POST',
+        path: '/items',
+        handler: async (request, h) => {
+            const { error, value } = crudItemSchema.validate(request.payload, { abortEarly: false });
+
+            if (error) {
+                const errors = error.details.map(err => ({
+                    field: err.context?.key,
+                    message: err.message
+                }));
+
+                return h.response({ errors }).code(400);
+            }
+
+            const { name, price } = value;
+            const item = await ItemService.createItem(name, price);
+            return h.response(item).code(201);
+        }
+    },
+    {
         method: 'PUT',
         path: '/items/{id}',
         handler: async (request, h) => {
             const id = parseInt(request.params.id);
-            const { name, price } = request.payload as { name: string; price: number };
+            const { error, value } = crudItemSchema.validate(request.payload, { abortEarly: false });
+
+            if (error) {
+                const errors = error.details.map(err => ({
+                    field: err.context?.key,
+                    message: err.message
+                }));
+                return h.response({ errors }).code(400);
+            }
+
+            const { name, price } = value;
 
             try {
                 return await ItemService.updateItem(id, name, price);
-            } catch (error: any) {
-                if (error.code === 'P2025') {
+            } catch (err: any) {
+                if (err.code === 'P2025') {
                     return h.response({ message: 'Item not found' }).code(404);
                 }
-                console.error(error);
                 return h.response({ message: 'Internal Server Error' }).code(500);
             }
         }
@@ -67,7 +88,7 @@ export const defineRoutes: ServerRoute[] = [
 
             try {
                 await ItemService.deleteItem(id);
-                return h.response().code(204); // No Content
+                return h.response().code(204);
             } catch (error: any) {
                 if (error.code === 'P2025') {
                     return h.response({ message: 'Item not found' }).code(404);
