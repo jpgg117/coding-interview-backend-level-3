fa327c16683dea3f3b8bacb3ad10c115f296f221
juanpablogarcia <juanpablogarcia78@hotmail.com>
2025-04-20 21:39:14 -0500

    add services and routes to pass basic tests

--- src/services/item.service.ts
+++ src/services/item.service.ts
@@ -2,9 +2,25 @@ import { prisma } from '../prisma';
 
 export const getAllItems = () => prisma.item.findMany();
 
+export const getItemById = (id: number) =>
+    prisma.item.findUnique({
+        where: { id }
+    });
+
 export const createItem = (name: string, price: number) =>
     prisma.item.create({
         data: { name, price }
     });
 
-export const clearItems = () => prisma.item.deleteMany(); // útil para tests
+export const updateItem = (id: number, name: string, price: number) =>
+    prisma.item.update({
+        where: { id },
+        data: { name, price }
+    });
+
+export const deleteItem = (id: number) =>
+    prisma.item.delete({
+        where: { id }
+    });
+
+export const clearItems = () => prisma.item.deleteMany();
